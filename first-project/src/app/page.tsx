f46003200c1ae70f9b662e2ca4334f1dd798299d
Naresh Khatri <nareshkhatri9810@gmail.com>
2025-02-12 21:11:49 +0545

    feat: update Home page text and enhance navigation links in Header component

--- first-project/src/app/page.tsx
+++ first-project/src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>This is next js</div>;
+  return <div>Home page</div>;
 }
--- first-project/src/components/Header.tsx
+++ first-project/src/components/Header.tsx
@@ -1,11 +1,19 @@
+import Link from "next/link";
+
 const NavBarPage = () => {
   return (
     <div>
       <nav className="w-full max-w-7xl mx-auto h-14 flex items-center justify-between">
+       <Link href={"/"}>
         <h1 className="text-xl font-bold">Home</h1>
+       </Link>
         <div className="flex gap-4">
+          <Link href={"/performance"} >
           <h1>Performance</h1>
-          <h1>Reliability</h1>
+          </Link>
+          <Link href={"/reliability"}>
+           <h1>Reliability</h1>
+          </Link>
         </div>
       </nav>
     </div>
